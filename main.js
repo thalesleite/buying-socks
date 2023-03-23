74c45c4b34f9834aaeb4a14f3f055b0ddb2f4afa
thalesleite <thalesaleite@gmail.com>
2023-03-23 16:30:39 +0000

    class and style binding

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = Vue.createApp({
       cart: 0,
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
-      inStock: true,
+      inStock: false,
       details: ["50% cotton", "30% wool", "20% plyester"],
       variants: [
         { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
